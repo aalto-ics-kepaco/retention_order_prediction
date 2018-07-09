f44bdf3c6112c2fb60b7c4bef5178b466e6b082b
bach <eric.bach@aalto.fi>
2018-07-09 19:53:03 +0300

    clean up

--- data/scripts/fingerprint_tools.R
+++ data/scripts/fingerprint_tools.R
@@ -1,13 +1,12 @@
+#
+# Function to calculate molecular fingerprints from SMILES and InChIs.
+#
+
 require (Matrix)
-require (igraph)
 require (obabel2R)
 require (rcdk)
 require (fingerprint)
 require (data.table)
-
-# Script to calculate the molecular fingerprints for each unique entry in
-# the retention-time database containing the molecules at the moment of
-# the publication.
 
 #' Calculate molecular fingerprints from inchi
 #' 
@@ -151,76 +150,6 @@ calculate_fingerprints_from_smiles <- function (
     }
 
     return (fps)
-}
-
-#' Function to parse one line fingerprint file created using MAYACHEMTOOLS
-#'
-#' @description 
-#'   
-#' @references
-#' MAYACHEMTOOLS: http://www.mayachemtools.org/
-#' 
-#' @param line string, one line from the fingerprint file
-#' @return matrix-row (1 x n_fingerprints), numeric, rowname corresponds to the
-#'         molecule id extracted from the line.
-parse_maya_fps_line <- function (line) {
-    stopifnot (is.character (line))
-    
-    line <- strsplit (line, ',')[[1]]
-
-    fps <- string_to_numeric_for_maya_feature_strings (line[2])
-    fps <- matrix (fps, nrow = 1)
-    
-    sample_id <- line[1]
-    rownames (fps) <- sample_id
-    
-    return (fps)
-}
-
-#' Functon to parse the feature string in a MAYACHEMTOOLS fingerprint line.
-#' 
-#' @references 
-#' MAYACHEMTOOLS: http://www.mayachemtools.org/
-#' 
-#' @return numeric vector of length n_fingerprints
-string_to_numeric_for_maya_feature_strings <- function (feature_str) {
-    fps <- strsplit (feature_str, ';')[[1]]
-    
-    is_binary <- (fps[1] == "FingerprintsBitVector") # Count: FingerprintsVector
-    
-    fps <- fps[length (fps)]
-    fps <- as.numeric (unlist (strsplit (fps, ifelse (is_binary, "", " "))))
-    
-    return (fps)
-}
-
-#' Function to read a list of fingerprints (csv-file) produced by the 
-#' MAYACHEMTOOLS. Each fingerprint will be parsed. A matrix containing the 
-#' fingerprints is returned and each row is named after using the first column 
-#' of the csv-file, e.g. the InChI.
-#' 
-#' @references 
-#' MAYACHEMTOOLS: http://www.mayachemtools.org/
-#' 
-#' @param filename string, filename of the csv-file
-#' @return matrix (n_samples x n_fingerprints), with rownames corresponding to 
-#'         the molecule ids.
-read_maya_fps_file_to_matrix <- function (filename) {
-    fps_df <- read.csv (filename, stringsAsFactors = FALSE)
-    n_samples <- nrow (fps_df)
-    
-    if (n_samples == 0) {
-        fps_mat <- matrix (NA, nrow = 0, ncol = 0)
-    } else {
-        n_fingerprints <- length (string_to_numeric_for_maya_feature_strings (fps_df[1, 2]))
-        fps_mat <- matrix (NA, nrow = n_samples, ncol = n_fingerprints)
-        for (idx in 1:n_samples) {
-            fps_mat[idx, ] <- string_to_numeric_for_maya_feature_strings (fps_df[idx, 2])
-        }
-        rownames (fps_mat) <- fps_df[, 1]
-    }
-    
-    return (fps_mat)
 }
 
 #' Concatenate list of fingerprints into a matrix
@@ -379,150 +308,4 @@ get_fingerprint_dimensions <- function (fps) {
     }
     
     return (fps_dimensions)
-}
-
-#' Produce a binary mask to exclude molecular fingerpints
-#' 
-#' @description 
-#' Given a binary fingerprint matrix a binary mask is produced,
-#' to exclude fingerprints:
-#' - with always the same value
-#' - which are redunant
-#' 
-#' @param fps binary matrix (or -1,1), shape (n_samples x n_fingerprints)
-#'        OR  list of fingerprints (1 x m) (with m = n_definitions)
-#'            list[[i]]: object of class \texttt{fingerprint}
-#' @param remove_single_value binary, exclude fps with always the same value
-#' @param remove_redundant binary, exclude redundant fps
-#' 
-#' @return binary vector (1 x n_fingerpints):
-#'         TRUE:  keep fingerprints
-#'         FALSE: exclude fingerprint
-get_fingerprint_mask <- function (
-    fps,
-    remove_single_value = TRUE,
-    remove_redundant    = TRUE) 
-{
-    # Fingerprint lists need to be converted to binary matrices first
-    if (is.list (fps)) {
-        fps <- fingerprints_to_matrix (fps)
-    }
-    
-    stopifnot (all (unique (as.vector (fps)) %in% c(TRUE, FALSE)) ||
-                   all (unique (as.vector (fps)) %in% c(-1, 1)))
-    
-    n_samples <- nrow (fps)
-    n_fps     <- ncol (fps)
-    
-    fps[fps == 0] <- -1
-    
-    # Find the columns in which all the fingerprints are either -1 or 1
-    if (remove_single_value) {
-        is_all_TRUE  <- apply (fps, MARGIN = 2, FUN = function (x) all (x ==  1))
-        is_all_FALSE <- apply (fps, MARGIN = 2, FUN = function (x) all (x == -1))
-    } else {
-        is_all_TRUE  <- rep (FALSE, n_fps)
-        is_all_FALSE <- rep (FALSE, n_fps)
-    }
-    
-    # Find all the redundant fingerprints:
-    #   fp_i = -1 <--> fp_j = -1
-    #   fp_i =  1 <--> fp_j =  1
-    if (remove_redundant) {
-        fps_cor <- (t(fps) %*% fps) / n_samples
-        
-        # Build up an undirected graph with edges between all redundant
-        # components
-        fps_graph <- graph_from_adjacency_matrix (fps_cor == 1, mode = "undirected")
-        # Find the connected components in this graph. For each component only
-        # one fingerprint definition needs to be kept. We keep the one with the
-        # lowest fingerprint index.
-        fps_comp <- components (fps_graph)
-        n_comp <- fps_comp$no
-        cols_not_redundant <- sapply (
-            1:n_comp, 
-            FUN = function (idx, fps_comp) { which (fps_comp$membership == idx)[1] }, 
-            fps_comp
-        )
-        is_redundant <- rep (TRUE, n_fps)
-        is_redundant[cols_not_redundant] <- FALSE
-    } else {
-        is_redundant <- rep (FALSE, n_fps)
-    }
-    
-    return (! (is_all_TRUE | is_all_FALSE | is_redundant))
-}
-
-#' Produce a binary mask to exclude molecular fingerprints
-get_count_fingerprint_mask <- function (count_fps) 
-{
-    is_all_ZERO <- apply (count_fps, MARGIN = 2, FUN = function (x) all (x == 0))
-    
-    return (! is_all_ZERO)
-}
-
-#' Function to remove certain redundant and unused fingerprints
-#' @param: fps, matrix (n_samples x n_fingerprints)
-#' @return matrix (n_samples x n_fingerprints')
-clean_up_molecular_fingerprints <- function (
-    bin_fps,
-    remove_single_value = TRUE,
-    remove_redundant    = TRUE) 
-{
-    tictoc::tic ("Pre-process the fingerprints")
-    
-    n_samples <- nrow (fps)
-    n_fps     <- ncol (fps)
-
-    # Find the columns in which all the fingerprints are either -1 or 1
-    fps[fps == 0] <- -1
-
-    cols_all_TRUE  <- apply (fps, MARGIN = 2, FUN = function (x) all (x ==  1))
-    cols_all_FALSE <- apply (fps, MARGIN = 2, FUN = function (x) all (x == -1))
-
-    stopifnot (length (cols_all_TRUE)  == n_fps)
-    stopifnot (length (cols_all_FALSE) == n_fps)
-
-    fps <- fps[, ! (cols_all_TRUE | cols_all_FALSE)]
-    n_fps <- ncol (fps)
-
-    # Find all the redundant fingerprints:
-    #   fp_i = -1 <--> fp_j = -1
-    #   fp_i =  1 <--> fp_j =  1
-    fps_cor <- (t(fps) %*% fps) / n_samples
-
-    stopifnot (all (diag (fps_cor) == 1))
-    stopifnot (all (dim (fps_cor) == n_fps))
-    # 'fps.ALL.cor'_ij is equal one iff fp_i and fp_j are redundant
-
-    # Build up an undirected graph with edges between all redundant
-    # components
-    fps.ALL.graph <- graph_from_adjacency_matrix (mat.fps.ALL.cor == 1, mode = "undirected")
-    # Find the connected components in this graph. For each component only
-    # one fingerprint definition needs to be kept. We keep the one with the
-    # lowest fingerprint index.
-    fps.ALL.comp <- components (fps.ALL.graph)
-    fps.ALL.selection <- sapply (1:fps.ALL.comp$no,
-                                 FUN = function (c.idx, fps.ALL.comp) { which (fps.ALL.comp$membership == c.idx)[1] },
-                                 fps.ALL.comp)
-    mat.fps.ALL <- mat.fps.ALL[, fps.ALL.selection]
-    n.fps <- ncol (mat.fps.ALL)
-
-    # Let us check whether the desired properties are fullfilled
-    stopifnot (! any (apply (mat.fps.ALL, MARGIN = 2, FUN = function (x) all (x ==  1))))
-    stopifnot (! any (apply (mat.fps.ALL, MARGIN = 2, FUN = function (x) all (x == -1))))
-
-    mat.fps.ALL.cor <- (t(mat.fps.ALL) %*% mat.fps.ALL) / n.mol
-
-    stopifnot (all (diag (mat.fps.ALL.cor) == 1))
-    stopifnot (all (dim (mat.fps.ALL.cor) == n.fps))
-    stopifnot (sum (mat.fps.ALL.cor == 1) == n.fps)
-
-    mat.fps.ALL[mat.fps.ALL == -1] <- 0
-
-    tictoc::toc (log = TRUE)
-
-    stopifnot (strcmp (rownames (mat.fps.ALL), smiles))
-
-    return (data.table (mat.fps.ALL, smiles = smiles))
 }
